19afa45ddf3b8b34e4ed787bf74748369b6fff49
Ailken <ailkencf@gmail.com>
2024-08-13 15:44:17 -0300

    ChessException and ChessPosition

---
+++ src/chess/chess/Position.java
@@ -0,0 +1,14 @@
+package chess;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+public class Position {
+
+    public Position(int i, int i0) {
+    }
+
+}
